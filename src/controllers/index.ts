aa0627182bd1b46b96c3df2cea13fa118f5bcd4a
kallyas <kallyasmedia@gmail.com>
2025-09-29 16:46:48 +0300

    feat: Expose controllers via index for easier import
    This commit introduces an index file for controllers, exporting all
    controllers (authController, userController, chatroomController, and
    messageController) as named exports. This simplifies imports throughout
    the application by allowing a single import statement from the
    `src/controllers` directory to access all available controllers,
    improving code organization and readability.
    
    Signed-off-by: kallyas <kallyasmedia@gmail.com>

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export * as authController from './authController';
+export * as userController from './userController';
+export * as chatroomController from './chatroomController';
+export * as messageController from './messageController';
